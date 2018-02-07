8bb36ba2241d3057b2aaafd8264ef343e37deb70
AldrinMac <aldrin.bonoo@gmail.com>
2018-02-07 16:30:45 +0100

    available volumes report timestamps its output

--- ec2-volumes.sh
+++ ec2-volumes.sh
@@ -2,4 +2,4 @@
 
 set -euo pipefail
 
-aws --output json ec2 describe-volumes --filters Name=status,Values=available > $1/volumes-available.json
+aws --output json ec2 describe-volumes --filters Name=status,Values=available > $1/volumes-available-$(date +%s).json
